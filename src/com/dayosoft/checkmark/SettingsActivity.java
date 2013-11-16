eb59b302f8b0dc895b4945b9bdc69468f79aa9a4
Joseph Dayo <joseph.dayo@gmail.com>
2013-11-16 21:13:35 +0800

    FB login WIP

---
+++ src/com/dayosoft/checkmark/SettingsActivity.java
@@ -0,0 +1,52 @@
+package com.dayosoft.checkmark;
+
+import com.dayosoft.checkmark.util.LoginHelper;
+import com.facebook.Session;
+import com.facebook.UiLifecycleHelper;
+import com.facebook.Session.StatusCallback;
+import com.facebook.SessionState;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SettingsActivity extends Activity implements StatusCallback {
+	
+	private UiLifecycleHelper uiHelper;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_settings);
+		
+		uiHelper = new UiLifecycleHelper(this, this);
+		uiHelper.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.settings, menu);
+		return true;
+	}
+
+	@Override
+	public void call(Session session, SessionState state, Exception exception) {
+		LoginHelper.onSessionStateChange(this, session, state, exception);
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		uiHelper.onActivityResult(requestCode, resultCode, data);
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		uiHelper.onDestroy();
+	}
+
+
+}
